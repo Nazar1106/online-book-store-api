591f2e4bfdba3eb3cfb71659fa8c2d7a6fe06260
kupnovytskyi <kupnovytskyinazar@gmail.com>
2024-12-30 16:34:04 +0200

    removed empty line and added RequestMapping to controller

--- src/main/java/com/example/bookstoreapp/controller/BookController.java
+++ src/main/java/com/example/bookstoreapp/controller/BookController.java
@@ -9,25 +9,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/books")
 public class BookController {
 
     private final BookService bookService;
 
-    @GetMapping("/books")
+    @GetMapping
     public List<BookDto> getAll() {
         return bookService.findAll();
     }
 
-    @GetMapping("books/{id}")
+    @GetMapping("/{id}")
     public BookDto getBookById(@PathVariable Long id) {
         return bookService.getBookById(id);
     }
 
-    @PostMapping("/books")
+    @PostMapping
     public BookDto createBook(@RequestBody CreateBookRequestDto bookDto) {
         return bookService.save(bookDto);
     }
