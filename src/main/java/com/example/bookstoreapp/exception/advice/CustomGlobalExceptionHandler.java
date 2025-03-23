60cac2eb9b84d7f79be1dd8672d351ddcb9f4363
kupnovytskyi <kupnovytskyinazar@gmail.com>
2025-03-23 21:47:03 +0200

    improved exception handler

--- src/main/java/com/example/bookstoreapp/exception/advice/CustomGlobalExceptionHandler.java
+++ src/main/java/com/example/bookstoreapp/exception/advice/CustomGlobalExceptionHandler.java
@@ -5,12 +5,15 @@ import com.example.bookstoreapp.exception.EntityNotFoundException;
 import com.example.bookstoreapp.exception.OrderProcessingException;
 import com.example.bookstoreapp.exception.RegistrationException;
 import java.time.ZonedDateTime;
+import java.util.stream.Collectors;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
@@ -21,48 +24,101 @@ public class CustomGlobalExceptionHandler {
     public static final String VALIDATION_EXCEPTION_MSG = "Validation exception";
     public static final String NO_RESOURCE_FOUND_MSG = "The resource doesn't exist";
     public static final String RESOURCE_CONFLICT = "The resource conflict";
-    public static final String ORDER_PROCESSING_EXCEPTION = "Can't do order, "
-            + "check your shopping cart";
+    public static final String AUTHENTICATION_EXCEPTION = "Authentication exception";
+    public static final String ORDER_PROCESSING_EXCEPTION = "Order could not be processed";
+    public static final String ENTITY_NOT_FOUND_EXCEPTION = "Entity not found exception";
+    public static final String AUTHORIZATION_EXCEPTION = "Authorization exception";
 
-    @ExceptionHandler(value = {MethodArgumentNotValidException.class,
-            MethodArgumentTypeMismatchException.class, DataIntegrityViolationException.class})
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<ApiError> handleValidationException() {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handleValidationException(MethodArgumentNotValidException ex) {
+        String collect = ex.getBindingResult().getFieldErrors().stream()
+                .map((DefaultMessageSourceResolvable::getDefaultMessage))
+                .collect(Collectors.joining());
+
         ApiError apiError = new ApiError(
                 VALIDATION_EXCEPTION_MSG,
+                collect,
                 ZonedDateTime.now()
         );
         return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(value = {NoResourceFoundException.class})
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<ApiError> handleNoResourceException() {
+    @ExceptionHandler(value = {OrderProcessingException.class})
+    public ResponseEntity<ApiError> handleProcessingException(OrderProcessingException ex) {
         ApiError apiError = new ApiError(
-                NO_RESOURCE_FOUND_MSG,
-                ZonedDateTime.now()
-        );
-        return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+                ORDER_PROCESSING_EXCEPTION,
+                ex.getMessage(),
+                ZonedDateTime.now());
+        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(value = {EntityNotFoundException.class})
-    public ResponseEntity<ApiError> handleEntityNotFoundException(EntityNotFoundException ex) {
-        ApiError apiError = new ApiError(ex.getMessage(),
-                ZonedDateTime.now());
-        return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ApiError> handleMismatchException(
+            MethodArgumentTypeMismatchException ex) {
+        ApiError apiError = new ApiError(
+                VALIDATION_EXCEPTION_MSG,
+                ex.getMessage(),
+                ZonedDateTime.now()
+        );
+        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ApiError> handleDataIntegrityViolationException(
+            DataIntegrityViolationException ex) {
+        ApiError apiError = new ApiError(
+                VALIDATION_EXCEPTION_MSG,
+                ex.getMessage(),
+                ZonedDateTime.now()
+        );
+        return new ResponseEntity<>(apiError, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(value = {RegistrationException.class})
-    public ResponseEntity<ApiError> handleRegistrationException() {
-        ApiError apiError = new ApiError(RESOURCE_CONFLICT,
+    public ResponseEntity<ApiError> handleRegistrationException(RegistrationException ex) {
+        ApiError apiError = new ApiError(
+                RESOURCE_CONFLICT,
+                ex.getMessage(),
                 ZonedDateTime.now());
         return new ResponseEntity<>(apiError, HttpStatus.CONFLICT);
     }
 
-    @ExceptionHandler(value = {OrderProcessingException.class})
-    public ResponseEntity<ApiError> handleProcessingException() {
-        ApiError apiError = new ApiError(ORDER_PROCESSING_EXCEPTION,
+    @ExceptionHandler(value = {AuthorizationDeniedException.class})
+    public ResponseEntity<ApiError> handleAuthorizationException(AuthorizationDeniedException ex) {
+        ApiError apiError = new ApiError(
+                AUTHORIZATION_EXCEPTION,
+                ex.getMessage(),
                 ZonedDateTime.now());
-        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(apiError, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(InternalAuthenticationServiceException.class)
+    public ResponseEntity<ApiError> handleAuthenticationException(
+            InternalAuthenticationServiceException ex) {
+        ApiError apiError = new ApiError(
+                AUTHENTICATION_EXCEPTION,
+                ex.getMessage(),
+                ZonedDateTime.now());
+
+        return new ResponseEntity<>(apiError, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(value = {NoResourceFoundException.class})
+    public ResponseEntity<ApiError> handleNoResourceException(NoResourceFoundException ex) {
+        ApiError apiError = new ApiError(
+                NO_RESOURCE_FOUND_MSG,
+                ex.getMessage(),
+                ZonedDateTime.now()
+        );
+        return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ApiError> handleEntityNotFoundException(EntityNotFoundException ex) {
+        ApiError apiError = new ApiError(
+                ENTITY_NOT_FOUND_EXCEPTION,
+                ex.getMessage(),
+                ZonedDateTime.now());
+        return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
     }
 }
